baa68b7b40dfaae11d78cfba2ac89892866a4060
AliElsaeid <120056055+AliElsaeid@users.noreply.github.com>
2023-05-21 18:45:55 +0300

    reset password func

--- routes/private/api.js
+++ routes/private/api.js
@@ -15,28 +15,30 @@ const getUser = async function (req) {
     .where("token", sessionToken)
     .innerJoin(
       "se_project.users",
-      "se_project.sessions.userid",
+      "se_project.sessions.userId",
       "se_project.users.id"
     )
     .innerJoin(
       "se_project.roles",
-      "se_project.users.roleid",
+      "se_project.users.roleId",
       "se_project.roles.id"
     )
    .first();
 
   console.log("user =>", user);
-  user.isNormal = user.roleid === roles.user;
-  user.isAdmin = user.roleid === roles.admin;
-  user.isSenior = user.roleid === roles.senior;
+  user.isNormal = user.roleId === roles.user;
+  user.isAdmin = user.roleId === roles.admin;
+  user.isSenior = user.roleId === roles.senior;
   return user;
 };
 
 module.exports = function (app) {
   // example
-  app.get("/users", async function (req, res) {
+  app.put("/users", async function (req, res) {
     try {
        const user = await getUser(req);
+     // const {userId}=req.body
+     console.log("hiiiiiiiiiii");
       const users = await db.select('*').from("se_project.users")
         
       return res.status(200).json(users);
@@ -44,9 +46,40 @@ module.exports = function (app) {
       console.log(e.message);
       return res.status(400).send("Could not get users");
     }
-   
   });
  
+  app.put("/api/v1/password/reset", async function (req, res) {
+    const { newPassword } = req.body;
+
+    // Check if the new password is provided
+    if (!newPassword) {
+      return res.status(400).send("New password is required");
+    }
+
+    try {
+      // Get the user from the session token
+      const sessionToken = req.cookies.session_token;
+      const session = await db
+        .select("userId")
+        .from("se_project.sessions")
+        .where("token", sessionToken)
+        .first();
+
+      if (!session) {
+        return res.status(401).send("Invalid session");
+      }
+
+      // Update the user's password in the database
+      await db("se_project.users")
+        .where("id", session.userId)
+        .update({ password: newPassword });
+
+      return res.status(200).send("Password reset successful");
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).send("Internal server error");
+    }
+  });
 
 
   
