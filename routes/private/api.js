0d2b88c48562b38f935cc2a36b83cb4d574d2828
AliElsaeid <120056055+AliElsaeid@users.noreply.github.com>
2023-05-26 19:11:03 +0300

    senior request func

--- routes/private/api.js
+++ routes/private/api.js
@@ -432,13 +432,53 @@ catch(error) {
   return res.status(500).send("Error processing the request");
 
 }
-//insert command table senior request - nationalid and user id i retrieved 
+  });
+  
+  app.post("/api/v1/senior/request", async (req, res) => {
+    const { nationalid } = req.body;
+//check nationalid
+  if (!nationalid) {
+    return res.status(400).send("National ID is required");
+  }
+//get user id from the current session
+try{
+  const session_token =getSessionToken(req);
+   const session = await db
+  .select("*")
+  .from("se_project.sessions")
+  .where("token", session_token)
+  .first();
+
+if(!session)
+  return res.status(401).send("Invalid session");
+
+
+
+//create senior request
+const seniorRequest = {
+  status: "Pending",
+  userid:session.userid,
+  nationalid: nationalid,
+};
+
+await db("se_project.senior_requests").insert(seniorRequest);
+
+return res.status(200).send("Senior request submitted");
+}
+catch(error) {
+  console.error(error);
+  return res.status(500).send("Error processing the request");
+
+}
+  });
+
+  
+  //insert command table senior request - nationalid and user id i retrieved 
 //add a pending status
 //insert into table-requests(status,userid,nationalid)
 //values(add values)
 //return star -----> checkkk <-----
 //try and catch --- console.log err.message(result.ststus 500)
-  });
   app.put("/api/v1/ride/simulate", async (req, res) => {
     try {
       const { origin, destination, tripDate } = req.body;
